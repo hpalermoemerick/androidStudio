ff143b743cea150a88cad714b8d3c200422c3cf5
hpalermoemerick <124798752+hpalermoemerick@users.noreply.github.com>
2023-11-29 22:44:50 -0300

    Update MainActivity.java

--- SQLite/MainActivity.java
+++ SQLite/MainActivity.java
@@ -1,24 +1,54 @@
-package com.example.atv_sqlite;
+package com.example.sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnCadatro, buttonListar;
+    private Button buttonCadastrar, buttonListar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCadatro = findViewById(R.id.buttonCadastrar);
-        btnCadatro.setOnClickListener(new View.OnClickListener() {
+//        SQLiteDatabase bancodedados = openOrCreateDatabase("app", MODE_PRIVATE, null);
+//
+//        bancodedados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (nome VARCHAR," + " idade int(3))");
+
+        //inserindo dados na tabela
+//        bancodedados.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Pedro', 17)");
+//        bancodedados.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Thiaguin', 18)");
+//
+//        //recuperar dados
+//        String consulta = "SELECT nome, idade FROM pessoas";
+//        Cursor cursor = bancodedados.rawQuery(consulta, null);
+//
+//        //indices de tabelas
+//        int indiceNome = cursor.getColumnIndex("nome");
+//        int indiceIdade = cursor.getColumnIndex("idade");
+//
+//        cursor.moveToFirst();
+//        String nome;
+//        int idade;
+//        while (cursor != null){
+//            nome = cursor.getString(indiceNome);
+//            idade = cursor.getInt(indiceIdade);
+//            Log.i("Resultado - Nome: ", nome);
+//            Log.i("Resultado - Idade: ", cursor.getString(indiceIdade));
+//            cursor.moveToNext();
+//        }
+
+        buttonCadastrar = findViewById(R.id.buttonCadastrar);
+        buttonCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 abrirTelaCadastro();
@@ -35,12 +65,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void abrirTelaCadastro() {
-        Intent telaCadastro = new Intent(this, CadastrarDados.class);
+        Intent telaCadastro = new Intent(this, Cadastro.class);
         startActivity(telaCadastro);
     }
 
     public void abrirTelaListagem() {
-        Intent telaListagem = new Intent(this, MostrarDados.class);
+        Intent telaListagem = new Intent(this, Listagem.class);
         startActivity(telaListagem);
     }
 }
